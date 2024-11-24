e75c15c9ef1c3ddcf7541451b59e08a84925cd36
Your Name <dilanesterff@gmail.com>
2024-11-24 14:57:11 +0300

    Initial commit

---
+++ src/Water.java
@@ -0,0 +1,30 @@
+public class Water extends GameUnit{
+
+    @Override
+    public double damage(GameUnit GU) {
+        if (GU instanceof Fire){
+            coef = 4;
+        }
+        else if (GU instanceof  Water) {
+            coef = 0;
+        }
+        else if (GU instanceof  Wind) {
+            coef = 1;
+        }
+        else if (GU instanceof  Ground) {
+            coef = 2;
+        }
+        return super.damage(GU);
+    }
+
+    @Override
+    public void setPhrase(String phrase) {
+        this.phrase = "Я - Вода, Я - H2O";
+    }
+
+
+    public Water(String name,double strength, double hp, double height, double weight, String type) {
+        super(name, strength, hp, height, weight, type);
+
+    }
+}
